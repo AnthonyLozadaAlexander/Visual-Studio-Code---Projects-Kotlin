cf0b2b95220a0fa56a6fcde90a5baa92e64799d9
zDefcon20 <antholozadarequelme@gmail.com>
2025-01-10 04:05:26 -0500

    update commit

---
+++ Condicionales/CondicionalWhen.kt
@@ -0,0 +1,21 @@
+import java.util.Scanner
+
+fun mainWhen(){
+  //Switch para los dias de la semana
+  var dia: Int
+  val input = Scanner(System.`in`)
+  println("Ingrese un dia de la semana: [1,2,3,4,5,6,7]\n")
+  dia = input.nextInt()
+
+  when(dia){
+    1 -> println("Lunes")
+    2 -> println("Martes")
+    3 -> println("Miercoles")
+    4 -> println("Jueves")
+    5 -> println("Viernes")
+    6 -> println("Sabado")
+    7 -> println("Domingo")
+    else -> println("Dia no valido")
+  }
+
+}
