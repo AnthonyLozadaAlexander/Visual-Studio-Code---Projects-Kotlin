c333dea5929f068aee49a2878e5a1f6c4af6d7f6
zDefcon20 <antholozadarequelme@gmail.com>
2025-01-10 04:36:08 -0500

    update commit

---
+++ Bucles/Suma.kt
@@ -0,0 +1,18 @@
+import java.util.Scanner
+import kotlin.intArrayOf
+
+fun main(){
+  var input = Scanner(System.`in`)
+  var suma = 0
+  var num: Int
+
+  println("Ingrese hasta que numero desea sumar") 
+  num = input.nextInt()
+
+  for (i in 1.. num){
+    println("$suma + $i")
+    suma = suma + i
+    println("= $suma")
+    
+  }
+}
