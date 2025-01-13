ad5b09b06d24accde9ef8261606bf576bfe5afd5
zDefcon20 <antholozadarequelme@gmail.com>
2025-01-12 23:55:59 -0500

    update commit

--- Ejercicios/ejercicio5.kt
+++ Ejercicios/ejercicio5.kt
@@ -3,14 +3,14 @@ fun main(){
 
   for (i in 1..100)
   {
-    if(i % 3 == 0 && i % 5 == 0){
-      println("$i = FizzBuzz")
-    }
-    else if(i % 3 == 0){
+    if(i % 3 == 0){
       println("$i = Fizz")
     }
     else if(i % 5 == 0){
       println("$i = Buzz")
+    }
+    if(i % 3 == 0 && i % 5 == 0){
+      println("$i = FizzBuzz")
     }
     else{
       println(i)
